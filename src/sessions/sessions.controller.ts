85510cec953ef26e4835ab3ab820275708e6e9ab
ezy4me <mrronal@mail.ru>
2023-08-17 20:53:39 +0300

    feature: add session crud

--- src/sessions/sessions.controller.ts
+++ src/sessions/sessions.controller.ts
@@ -1,8 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Put,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
 
 import { SessionsService } from './sessions.service';
+import { SessionDto } from './dto';
+import { Session } from '@prisma/client';
 
 @Controller('sessions')
 export class SessionsController {
   constructor(private readonly sessionService: SessionsService) {}
+
+  @Post()
+  async createSession(@Body() dto: SessionDto): Promise<Session> {
+    return this.sessionService.create(dto);
+  }
+
+  @Get()
+  async findAllSessions(): Promise<Session[]> {
+    return this.sessionService.findAll();
+  }
+
+  @Get(':id')
+  async findOneSessionById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Session> {
+    return this.sessionService.findOneById(id);
+  }
+
+  @Delete(':id')
+  async deleteSession(@Param('id', ParseIntPipe) id: number): Promise<Session> {
+    return this.sessionService.delete(id);
+  }
+
+  @Put(':id')
+  async updateSession(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: SessionDto,
+  ): Promise<Session> {
+    return this.sessionService.update(id, dto);
+  }
 }
--- src/sessions/sessions.service.ts
+++ src/sessions/sessions.service.ts
@@ -1,4 +1,75 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { SessionDto } from './dto';
+import { Session } from '@prisma/client';
 
 @Injectable()
-export class SessionsService {}
+export class SessionsService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(dto: SessionDto): Promise<Session> {
+    const session = await this.isUnique(dto);
+
+    if (session) {
+      return this.databaseService.session.create({
+        data: dto,
+      });
+    }
+  }
+
+  async findAll(): Promise<Session[]> {
+    return this.databaseService.session.findMany();
+  }
+
+  async findOneById(id: number): Promise<Session> {
+    const session = await this.databaseService.session.findUnique({
+      where: { id },
+    });
+
+    if (!session) {
+      throw new NotFoundException(`Session with id ${id} not found`);
+    }
+
+    return session;
+  }
+
+  async delete(id: number): Promise<Session> {
+    const session = await this.findOneById(id);
+
+    if (session) {
+      return this.databaseService.session.delete({
+        where: { id },
+      });
+    }
+  }
+
+  async update(id: number, dto: SessionDto): Promise<Session> {
+    const session = await this.findOneById(id);
+    const isUnique = await this.isUnique(dto);
+
+    if (session && isUnique) {
+      return this.databaseService.session.update({
+        where: { id },
+        data: dto,
+      });
+    }
+  }
+
+  private async isUnique(dto: SessionDto): Promise<boolean> {
+    const { startTime, date, movieId, sessionTypeId, hallId } = dto;
+
+    const session = await this.databaseService.session.findFirst({
+      where: { startTime, date, movieId, sessionTypeId, hallId },
+    });
+
+    if (session) {
+      throw new BadRequestException(`Session with data: ${dto} is exist`);
+    }
+
+    return true;
+  }
+}
